b5976846f62d0c74af07ea0545ad3bc02732a7d1
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2019-06-17 16:15:22 -0300

    "Add example setting files for Clus"

---
+++ experiments/clus_experiments/instruments_example.s
@@ -0,0 +1,17 @@
+
+[Data]
+File = data/instruments.arff
+TestSet = 0.3
+
+[Hierarchical]
+Type = TREE
+HSeparator = /
+
+[Tree]
+
+[Model]
+MinimalWeight = 1.0
+
+[Output]
+WritePredictions = {Test}
+
---
+++ experiments/clus_experiments/mosquitos_example.s
@@ -0,0 +1,17 @@
+
+[Data]
+File = data/mosquitos.arff
+TestSet = 0.3
+
+[Hierarchical]
+Type = TREE
+HSeparator = .
+
+[Tree]
+
+[Model]
+MinimalWeight = 1.0
+
+[Output]
+WritePredictions = {Test}
+
